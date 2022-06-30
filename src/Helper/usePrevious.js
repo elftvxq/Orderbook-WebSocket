731bb665061816a0098075abd6d305cd7e49a1e6
Emma Shih <emma.shih@shopee.com>
2022-06-30 17:47:58 +0800

    adjust comment

--- src/Helper/usePrevious.js
+++ src/Helper/usePrevious.js
@@ -3,8 +3,11 @@ import React, { useRef, useEffect } from 'react';
 const usePrevious = (value) => {
   const ref = useRef();
   useEffect(() => {
-    ref.current = value; //assign the value of ref to the argument
+    //assign the value of ref to the argument
+    ref.current = value;
   }, [value]); //this code will run when the value of 'value' changes
-  return ref.current; //in the end, return the current ref value.
+
+  //in the end, return the current ref value.
+  return ref.current;
 };
 export default usePrevious;
