84fbd81f4446c5d73253dbd4e0ef75d9ecc3a4a7
Emma Shih <emma.shih@shopee.com>
2022-06-30 16:34:56 +0800

    update quotes as requirements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.scss';
-import React, { useState, useEffect } from 'react';
-
-const data = JSON.parse(
-  '{"topic":"orderBookApi","data":{"buyQuote":[{"price":"21426.5","size":"35"},{"price":"21424.0","size":"83"},{"price":"21421.0","size":"583"},{"price":"21414.5","size":"1602"},{"price":"21413.0","size":"1327"},{"price":"21412.5","size":"2813"},{"price":"21412.0","size":"20"},{"price":"21410.0","size":"4845"},{"price":"21408.5","size":"2313"},{"price":"21408.0","size":"1760"},{"price":"21406.0","size":"2633"},{"price":"21405.5","size":"2238"},{"price":"21405.0","size":"4710"},{"price":"21403.5","size":"7396"},{"price":"21402.5","size":"4640"},{"price":"21401.0","size":"8132"},{"price":"21400.5","size":"3883"},{"price":"21400.0","size":"16"},{"price":"21398.5","size":"589"},{"price":"21397.5","size":"1823"},{"price":"21397.0","size":"4623"},{"price":"21394.0","size":"3403"},{"price":"21393.5","size":"6394"},{"price":"21390.0","size":"4705"},{"price":"21389.5","size":"231"},{"price":"21387.5","size":"400"},{"price":"21386.5","size":"585"},{"price":"21385.5","size":"7407"},{"price":"21384.0","size":"1462"},{"price":"21382.0","size":"667"},{"price":"21381.5","size":"1499"},{"price":"21380.0","size":"356"},{"price":"21378.5","size":"1823"},{"price":"21377.0","size":"2135"},{"price":"21376.0","size":"2271"}],"sellQuote":[{"price":"21482.0","size":"2341"},{"price":"21481.5","size":"700"},{"price":"21480.5","size":"1507"},{"price":"21479.0","size":"2416"},{"price":"21477.5","size":"1363"},{"price":"21477.0","size":"465"},{"price":"21476.5","size":"2008"},{"price":"21473.5","size":"1454"},{"price":"21472.0","size":"5689"},{"price":"21471.0","size":"810"},{"price":"21468.5","size":"758"},{"price":"21468.0","size":"5715"},{"price":"21463.5","size":"10613"},{"price":"21460.0","size":"5471"},{"price":"21459.0","size":"2023"},{"price":"21456.5","size":"10412"},{"price":"21454.0","size":"5003"},{"price":"21453.0","size":"1451"},{"price":"21451.0","size":"4906"},{"price":"21450.0","size":"21192"},{"price":"21449.0","size":"863"},{"price":"21448.5","size":"2649"},{"price":"21447.5","size":"5411"},{"price":"21446.5","size":"946"},{"price":"21446.0","size":"2826"},{"price":"21445.5","size":"1882"},{"price":"21444.5","size":"3734"},{"price":"21443.0","size":"397"},{"price":"21441.5","size":"1440"},{"price":"21439.5","size":"467"},{"price":"21439.0","size":"560"},{"price":"21436.5","size":"667"},{"price":"21435.0","size":"20"},{"price":"21433.5","size":"53"},{"price":"21427.0","size":"755"}],"lastPrice":"21420.5","timestamp":1656252537765,"gain":1,"symbol":"BTCPFC"}}'
-);
-
-console.log(data);
+import usePrevious from './Helper/usePrevious';
+import { ReactComponent as ArrowIcon } from './Styles/assets/IconArrowDown.svg';
 
 function App() {
-  const [first, setfirst] = useState(0);
-  const ws = new WebSocket('wss://ws.btse.com/ws/futures');
+  const [sellQuotes, setSellQuotes] = useState([]);
+  const [buyQuotes, setBuyQuotes] = useState([]);
+  const [lastPrice, setLastPrice] = useState(0);
+
+  const [totalSellQuotes, setTotalSellQuotes] = useState(0);
+  const [totalBuyQuotes, setTotalBuyQuotes] = useState(0);
+  const [gain, setGain] = useState(0);
+
+  const prevSellQuote = usePrevious(sellQuotes);
+  const prevBuyQuote = usePrevious(buyQuotes);
+
+  const ws = new WebSocket(`wss://ws.btse.com/ws/futures`);
 
   useEffect(() => {
     ws.onopen = () => {
@@ -20,13 +26,30 @@ function App() {
         })
       );
 
-      console.log('sent');
+      // console.log('sent');
     };
+
     ws.onmessage = (event) => {
       console.log('receive from server');
-      console.log(event);
+      // console.log(event);
 
       let streamingData = JSON.parse(event.data);
+      console.log(streamingData, 'streamingData');
+
+      if (streamingData.data) {
+        const {
+          data: { buyQuote, sellQuote, gain, lastPrice },
+        } = streamingData;
+
+        let newBuyData = calculateAccumulativeTotal(buyQuote, 'buy');
+        setBuyQuotes(newBuyData);
+
+        let newSellData = calculateAccumulativeTotal(sellQuote, 'sell');
+        setSellQuotes(newSellData);
+
+        setLastPrice(lastPrice);
+        setGain(gain);
+      }
     };
     ws.onclose = () => {
       console.log('close connection');
@@ -35,6 +58,7 @@ function App() {
 
   const closeConnect = () => {
     ws.close();
+    console.log('close connection');
 
     ws.send(
       JSON.stringify({
@@ -42,21 +66,238 @@ function App() {
         args: ['orderBookApi:BTCPFC_0'],
       })
     );
-
-    console.log('close connection');
   };
+
+  const calculateAccumulativeTotal = (quoteDetail, quoteType) => {
+    let quotesResult = quoteDetail.slice(0, 8).map((item, index) => {
+      let accumulativeTotal = 0;
+      let totalValue = 0;
+
+      if (quoteType === 'buy') {
+        for (let i = index; i >= 0; i--) {
+          accumulativeTotal += parseInt(quoteDetail[i].size);
+          totalValue += quoteDetail[i].price * quoteDetail[i].size;
+        }
+        if (index === 7) {
+          // console.log(accumulativeTotal, 'accumulativeTotal');
+          // 設定當前最大量紀錄
+
+          const newTotalBuyQuote =
+            totalBuyQuotes > accumulativeTotal
+              ? totalBuyQuotes
+              : accumulativeTotal;
+          setTotalBuyQuotes(newTotalBuyQuote);
+        }
+      } else if (quoteType === 'sell') {
+        for (let i = index; i < 8; i++) {
+          accumulativeTotal += parseInt(quoteDetail[i].size);
+          totalValue += quoteDetail[i].price * quoteDetail[i].size;
+        }
+        if (index === 0) {
+          const newTotalSellQuote =
+            totalSellQuotes > accumulativeTotal
+              ? totalSellQuotes
+              : accumulativeTotal;
+          console.log(newTotalSellQuote, 'newTotalSellQuote');
+          console.log(totalSellQuotes, 'totalSellQuotes');
+          console.log(accumulativeTotal, 'accumulativeTotal');
+
+          setTotalSellQuotes(newTotalSellQuote);
+        }
+      }
+
+      const accumulativeTotalData = { ...item };
+
+      accumulativeTotalData.accumulativeTotal = accumulativeTotal;
+      accumulativeTotalData.totalValue = totalValue;
+
+      return accumulativeTotalData;
+    });
+
+    if (!quotesResult.length) return [];
+    // closeConnect();
+    return quotesResult;
+  };
+
+  const lastPriceColor = () => {
+    switch (gain) {
+      case 1:
+        return 'go-up';
+      case -1:
+        return 'go-down';
+      default:
+        return null;
+    }
+  };
+
+  useEffect(() => {
+    lastPriceColor();
+  }, [gain]);
+
+  const formatNumberWithThousandsSeparators = (num) => {
+    if (!num) return;
+    if (isNaN(num)) {
+      return num;
+    }
+    return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  // console.log(totalSellQuotes, 'totalSellQuotes');
+
+  useEffect(() => {
+    const newQuoteUpdate = (currQuotes, oldQuotes, quoteType) => {
+      // 新的 quote data 迴圈
+      currQuotes.forEach((quote, index) => {
+        const exist = oldQuotes.some((item) => {
+          if (item.price === quote.price) {
+            return true;
+          }
+          return false;
+        });
+
+        if (!exist) {
+          let newValue = [...currQuotes];
+          newValue[index].status = true;
+          if (quoteType === 'sellQuotes') {
+            setSellQuotes(newValue);
+          } else if (quoteType === 'buyQuotes') {
+            setBuyQuotes(newValue);
+          }
+        } else {
+          // 將已存在quote的size進行比對。
+          const oldQuotesIndex = oldQuotes.findIndex(
+            (item) => item.price === quote.price
+          );
+          let newValue = [...currQuotes];
+
+          if (quote.size > oldQuotes[oldQuotesIndex].size) {
+            newValue[index].sizeChange = 'size-up';
+            if (quoteType === 'sellQuotes') {
+              setSellQuotes(newValue);
+            } else if (quoteType === 'buyQuotes') {
+              setBuyQuotes(newValue);
+            }
+          } else if (quote.size < oldQuotes[oldQuotesIndex].size) {
+            newValue[index].sizeChange = 'size-down';
+            if (quoteType === 'sellQuotes') {
+              setSellQuotes(newValue);
+            } else if (quoteType === 'buyQuotes') {
+              setBuyQuotes(newValue);
+            }
+          }
+        }
+      });
+    };
+
+    if (prevSellQuote && prevSellQuote.length) {
+      newQuoteUpdate(sellQuotes, prevSellQuote, 'sellQuotes');
+    }
+
+    if (prevBuyQuote && prevBuyQuote.length) {
+      newQuoteUpdate(buyQuotes, prevBuyQuote, 'buyQuotes');
+    }
+  }, [prevSellQuote, prevBuyQuote, buyQuotes, sellQuotes]);
 
   return (
     <div className='App'>
-      <button onClick={closeConnect}>CLOSE BUTTON</button>
+      {/* <button onClick={closeConnect}>CLOSE BUTTON</button> */}
 
       <div className='OrderbookContainer'>
         <div className='OrderbookContainer__title'>Order Book</div>
-        <div className='OrderbookContainer__item'>
-          <div class='OrderbookContainer__item__header'>
-            <div class='OrderbookContainer__item__price'>Prize(USD)</div>
-            <div class='OrderbookContainer__item__size'>Size</div>
-            <div class='OrderbookContainer__item__total'>Total</div>
+
+        <div className='OrderbookContainer__box'>
+          <div className='OrderbookContainer__box__header'>
+            <div className='OrderbookContainer__item price'>Prize(USD)</div>
+            <div className='OrderbookContainer__item size'>Size</div>
+            <div className='OrderbookContainer__item total'>Total</div>
+          </div>
+          {/* sell quotes */}
+          <div className='QuotesContainer'>
+            {sellQuotes &&
+              sellQuotes.map((quote, index) => {
+                return (
+                  <div
+                    className={`QuotesContainer__row sell ${
+                      quote.status ? 'new-quote' : null
+                    }`}
+                    key={quote.timestamp}
+                  >
+                    <div className='QuotesContainer__row__item price'>
+                      {formatNumberWithThousandsSeparators(quote.price)}
+                    </div>
+                    <div
+                      className={`QuotesContainer__row__item size ${quote.sizeChange}`}
+                    >
+                      {formatNumberWithThousandsSeparators(quote.size)}
+                    </div>
+                    <div className='QuotesContainer__row__item total'>
+                      {formatNumberWithThousandsSeparators(
+                        quote.accumulativeTotal
+                      )}
+                      <div
+                        className='bar'
+                        style={{
+                          right: `${
+                            (quote.accumulativeTotal / totalSellQuotes) * 100
+                          }%`,
+                        }}
+                      >
+                        &nbsp;
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+
+          <div className={`LastPriceContainer ${lastPriceColor()}`}>
+            <div className={`LastPriceContainer__price `}>
+              {lastPrice && formatNumberWithThousandsSeparators(lastPrice)}
+              {gain !== 0 && (
+                <div className='arrow-icon'>
+                  <ArrowIcon />
+                </div>
+              )}
+            </div>
+          </div>
+          {/* buy quotes */}
+
+          <div className='QuotesContainer'>
+            {buyQuotes &&
+              buyQuotes.map((quote, index) => {
+                return (
+                  <div
+                    className={`QuotesContainer__row buy ${
+                      quote.status ? 'new-quote' : null
+                    }`}
+                    key={quote.timestamp}
+                  >
+                    <div className='QuotesContainer__row__item price'>
+                      {formatNumberWithThousandsSeparators(quote.price)}
+                    </div>
+                    <div
+                      className={`QuotesContainer__row__item size ${quote.sizeChange}`}
+                    >
+                      {formatNumberWithThousandsSeparators(quote.size)}
+                    </div>
+                    <div className='QuotesContainer__row__item total'>
+                      {formatNumberWithThousandsSeparators(
+                        quote.accumulativeTotal
+                      )}
+                      <div
+                        className='bar'
+                        style={{
+                          right: `${
+                            (quote.accumulativeTotal / totalBuyQuotes) * 100
+                          }%`,
+                        }}
+                      >
+                        &nbsp;
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
